cd3c6a526a1e9dc80d0b5a0605814c5c5ceee71a
zero8d <zero8d@yandex.ru>
2021-07-28 17:22:14 +0500

    Good state

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
 const TelegramBot = require("node-telegram-bot-api");
 const { forward } = require("./utils/fonts");
-// replace the value below with the Telegram token you receive from @BotFather
 const token = process.env.TELEGRAM_TOKEN;
 const convert = forward;
-// Create a bot that uses 'polling' to fetch new updates
 
 var port = process.env.PORT || 8443;
 var bot = new TelegramBot(token, { webHook: { port: port } });
 const url = process.env.APP_URL;
 bot.setWebHook(`${url}/bot${token}`);
-
-// Listen for any kind of message. There are different kinds of
-// messages.
 bot.on("message", async (msg) => {
   const chatId = msg.chat.id;
   if (msg.text ? msg.text.toLowerCase() !== "/start" : false) {
-    // send a message to the chat acknowledging receipt of their message
     const convertedText = convert(msg.text)
       .split(/\n/)
       .map((text) => {
