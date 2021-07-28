72069ea2843626b5ac97296f76033f1428dd1f96
zero8d <zero8d@yandex.ru>
2021-07-28 17:20:13 +0500

    Security issues

--- index.js
+++ index.js
@@ -1,30 +1,14 @@
 const TelegramBot = require("node-telegram-bot-api");
 const { forward } = require("./utils/fonts");
 // replace the value below with the Telegram token you receive from @BotFather
-const token =
-  process.env.TELEGRAM_TOKEN ||
-  "1934972123:AAFJGemQb0MtyfA0x9RPWntaVGpkFs1C5u8";
+const token = process.env.TELEGRAM_TOKEN;
 const convert = forward;
 // Create a bot that uses 'polling' to fetch new updates
 
 var port = process.env.PORT || 8443;
-var host = process.env.HOST;
 var bot = new TelegramBot(token, { webHook: { port: port } });
-const url = process.env.APP_URL || "https://beautynick.herokuapp.com:443";
+const url = process.env.APP_URL;
 bot.setWebHook(`${url}/bot${token}`);
-
-// Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
-
-  const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
-
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
-});
 
 // Listen for any kind of message. There are different kinds of
 // messages.
